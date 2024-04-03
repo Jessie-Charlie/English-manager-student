3bda8cd90b6005f26f17403c705b3f13cb0cfd39
Jessie-Charlie <2503239387@qq.com>
2024-04-03 22:52:15 +0800

    English-manager-student

---
+++ src/api/subject.js
@@ -0,0 +1,7 @@
+import { post } from '@/utils/request'
+
+export default {
+  // list: query => post('/api/student/education/subject/list'),
+  list: query => post('/api/student/education/subject/list'),
+  select: id => post('/api/student/education/subject/select/' + id)
+}
